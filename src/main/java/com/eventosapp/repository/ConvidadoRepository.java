cf77f3a4ae78339beec5deeadf73b477b4555d59
Unknown <hamudetrf@gmail.com>
2018-12-10 14:48:54 -0200

    endpoint convidadoRepositorio

--- src/main/java/com/eventosapp/repository/ConvidadoRepository.java
+++ src/main/java/com/eventosapp/repository/ConvidadoRepository.java
@@ -1,13 +1,25 @@
 package com.eventosapp.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.eventoapp.models.Convidado;
 import com.eventoapp.models.Evento;
 
-public interface ConvidadoRepository extends CrudRepository<Convidado, String>{// buscando lista de convidados do evento	 
+public interface ConvidadoRepository extends CrudRepository<Convidado, String> {// buscando lista de convidados do evento	 
 	// buscando no banco de dados atraves do evento
 	Iterable<Convidado> findByEvento(Evento evento);
 	Convidado findByRg(String rg);
+	//Convidado findByCodigo(long codigo);
+	Convidado findByNum(long num);
+//	Convidado findByCodigo(long codigo);
 	
+	@Query("select e from Convidado e  where evento_codigo=?1")
+	List <Convidado> findByCodigoEvento(long codigo);
+
+//	Convidado upConvidado(long num);
+
+
 }
