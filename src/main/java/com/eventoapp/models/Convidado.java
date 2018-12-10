a5de468fed94916bbfb347b4ee5506dab622028c
Unknown <hamudetrf@gmail.com>
2018-12-10 14:12:37 -0200

    endpoint modelConvidado

--- src/main/java/com/eventoapp/models/Convidado.java
+++ src/main/java/com/eventoapp/models/Convidado.java
@@ -2,16 +2,31 @@ package com.eventoapp.models;
 
 
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
-public class Convidado {
+//@NamedQuery(name="ordenaConvidado", query="Select a from convidado order by nome_convidado desc ")
+@Table(name="convidado")
+public class Convidado implements Serializable {
+	//private static final long serialVersionUID = 1L;
+	
 	
 	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO) // gerando um codigo automaticante do tipo long
+	private long num;
+	
+
+	//	@Id
 	@NotEmpty
 	private String rg;
 	
@@ -21,7 +36,21 @@ public class Convidado {
 	@ManyToOne // muitos convidados para um evento
 	private Evento evento;
 	
-
+	
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	
+	public long getNum() {
+		return num;
+	}
+	public void setNum(long num) {
+		this.num = num;
+	}
+	
 	
 	public Evento getEvento() {
 		return evento;
@@ -29,12 +58,7 @@ public class Convidado {
 	public void setEvento(Evento evento) {
 		this.evento = evento;
 	}
-	public String getRg() {
-		return rg;
-	}
-	public void setRg(String rg) {
-		this.rg = rg;
-	}
+
 	public String getNomeConvidado() {
 		return nomeConvidado;
 	}
